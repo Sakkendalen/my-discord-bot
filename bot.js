33142e5a0355972959149d569407b2e67e1f696a
Sakkendalen <Sakutynjala@gmail.com>
2021-03-22 22:31:48 +0200

    user mention test
    scope:
    -bot.js
    
    reason:
    -dissing our friends choice of game

--- bot.js
+++ bot.js
@@ -1,15 +1,34 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 require('dotenv').config();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('online!');
 });
 
+const Responses = [
+  "black desert on paska!",
+  "pylly on kakan koti",
+  "miehet pelaa talonroa",
+  "Korona on homosammakoiden salaliitto",
+  "oot nää idiootti"
+];
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === 'kerro totuus') {
+    const resp = Math.floor(Math.random() * Responses.length)
+    msg.reply(Responses[resp]);
   }
+
+  if (msg.content === "$loop") { 
+    console.log("loop aquired")
+    var interval = setInterval (function () {
+        // use the message's channel (TextChannel) to send a new message
+        msg.channel.send("<@185766256811442178>, sinäkin voisit olla pelaamassa talonroa. ps musta jälkiruoka on paska")
+        .catch(console.error); // add error handling here
+    }, 1 * 6000);
+}
 });
 
 client.login(process.env.API_KEY);
